9d42b67033cea91f8708305b3afc3848b6139194
Maciej Zygmanowski <sppmacd@pm.me>
2021-01-08 16:35:46 +0100

    Start making system code + add ascii font

---
+++ src/sys/krnl/boot.s
@@ -0,0 +1,55 @@
+	.file	"boot.c"
+	.text
+	.globl	test
+	.data
+	.align 32
+	.type	test, @object
+	.size	test, 40
+test:
+	.long	0
+	.long	1
+	.long	2
+	.long	3
+	.long	4
+	.long	5
+	.long	6
+	.long	7
+	.long	8
+	.long	9
+	.text
+	.globl	_start
+	.type	_start, @function
+_start:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	_start, .-_start
+	.ident	"GCC: (Ubuntu 10.2.0-5ubuntu1~20.04) 10.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
