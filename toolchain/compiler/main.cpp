6955d145b8c1e89bd3fd6e7b9dc057f10b0cd2cd
Maciej Zygmanowski <sppmacd@pm.me>
2021-01-09 16:48:35 +0100

    compiler: Implement more command-line arguments

--- toolchain/compiler/main.cpp
+++ toolchain/compiler/main.cpp
@@ -1,14 +1,24 @@
-#include <iostream>
-
 #include <config.h>
 #include <util/args.h>
+
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
     std::map<std::string, util::ArgSpec> argspec = {
-        {"help",    {true,  "Display help"}},
-        {"version", {true,  "Display version"}},
-        {"o",       {false, "Specify output file"}}
+        // Generic
+        {"help",    {true,  "Display help and quit"}},
+        {"version", {true,  "Display version and quit"}},
+        
+        // Output
+        {"o",       {false, "Specify output file"}},
+        
+        // Warning options
+        {"Wall",    {false, "Enable all warnings"}},
+        {"Werror",  {false, "Treat warnings as errors"}},
+        
+        // Dialect options
+        {"ffreestanding", {false, "Do not link libc and crt0 (useful for OS kernels)"}}
     };
     
     util::Args args = util::parse_args(argc, argv, argspec);
@@ -16,22 +26,40 @@ int main(int argc, char* argv[])
     if(args.is_error)
         return 1;
     
-    std::cout << "Options:" << std::endl;
-    for(auto& it: args.options)
-    {
-        std::cout << it.first << " = " << it.second << std::endl;
-    }
-    std::cout << "Positional arguments:" << std::endl;
-    for(auto& arg: args.positional_arguments)
-    {
-        std::cout << arg << ", " << std::endl;
-    }
-    
     if(args.options.count("version"))
     {
         std::cout << std::endl;
-        std::cout << argv[0] << " (SPP Compiler) " << SCC_VER << " for " << SCC_OS_NAME << " (" << SCC_OS_ARCH << ")" << std::endl << std::endl;
+        std::cout << argv[0] << " (SPP Compiler) " << SCC_VER << " for " << SCC_OS_NAME << " (" << SCC_OS_ARCH << ")" << std::endl;
+        std::cout << "Licensed under No license :)" << std::endl;
+        return 0;
     }
+    if(args.options.count("help"))
+    {
+        util::display_arg_help(argspec);
+        return 0;
+    }
+    
+    // Input file
+    std::string input;
+    if(args.positional_arguments.empty())
+    {
+        std::cout << "- No inputs specified!" << std::endl;
+        return 1;
+    }
+    else if(args.positional_arguments.size() > 1)
+    {
+        std::cout << "- More than 1 input file specified!" << std::endl;
+        return 1;
+    }
+    else
+        input = args.positional_arguments[0];
+    
+    // Output file
+    std::string output = args.options["o"];
+    if(output.empty())
+        output = "a.out";
+    
+    std::cout << "- Output file: " << output << std::endl;
     
     return 0;
 }
