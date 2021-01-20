c4a45589e48efbe66ab220088758c8e31da3ff36
Maciej Zygmanowski <sppmacd@pm.me>
2021-01-20 15:57:27 +0100

    shell: Fix

--- src/app/sh/main.c
+++ src/app/sh/main.c
@@ -31,33 +31,35 @@ int main(u16 argc, char* argv[])
             case -3:
                 perror("system error\n");
                 break;
-            case 0: continue;
-        }
-        
-        switch(bsearch(BUILTINS, sizeof(BUILTINS), argv[0])) {
             case 0:
-                if(argc < 1)
-                    break;
-                chdir(argv[1]);
-                break;
-            case 1:
-                return 0;
-            case 2:
-                puts("cd, exit, help\n");
-                break;
-            case 3:
-                if(argc < 1)
-                    break;
-                open(argv[1], FCREATE | FTOUCH | FDIR);
-                break;
-            case 4:
-                if(argc < 1)
-                    break;
-                open(argv[1], FCREATE | FTOUCH);
                 break;
             default:
-                if(spawn_wait(argv[0], argc, (const char**)(argv + 1)) < 0)
-                    perror("spawn");
+                switch(bsearch(BUILTINS, sizeof(BUILTINS), argv[0])) {
+                    case 0:
+                        if(argc < 1)
+                            break;
+                        chdir(argv[1]);
+                        break;
+                    case 1:
+                        return 0;
+                    case 2:
+                        puts("cd, exit, help\n");
+                        break;
+                    case 3:
+                        if(argc < 1)
+                            break;
+                        open(argv[1], FCREATE | FTOUCH | FDIR);
+                        break;
+                    case 4:
+                        if(argc < 1)
+                            break;
+                        open(argv[1], FCREATE | FTOUCH);
+                        break;
+                    default:
+                        if(spawn_wait(argv[0], argc, (const char**)(argv + 1)) < 0)
+                            perror("spawn");
+                        break;
+                }
                 break;
         }
     }
